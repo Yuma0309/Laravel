26c9a4f65e247f5fe75e173be568971074e023fa
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-08-25 08:48:05 +0900

    リスト2-6までの動作を確認

--- ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/MyClasses/MyService.php
+++ ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/MyClasses/MyService.php
@@ -1,23 +1,37 @@
 <?php
 namespace App\MyClasses;
 
+
 class MyService
 {
-    private $msg;
-    private $data;
+    private $id = -1;
+    private $msg = 'no id...';
+    private $data = ['Hello', 'Welcome', 'Bye'];
 
-    public function __construct()
+
+    public function __construct(int $id = -1)
     {
-        $this->msg = 'Hello! This is MyService!';
-        $this->data = ['Hello', 'Welcome', 'Bye'];
+        if ($id >= 0)
+        {
+            $this->id = $id;
+            $this->msg = 'select: ' . $this->data[$id];
+        }
     }
+
 
     public function say()
     {
         return $this->msg;
     }
 
-    public function data()
+
+    public function data(int $id)
+    {
+        return $this->data[$id];
+    }
+
+
+    public function alldata()
     {
         return $this->data;
     }
