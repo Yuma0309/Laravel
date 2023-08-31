6ec791aefc77f680a8926203e8b6d4b4db112860
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-08-31 09:09:58 +0900

    リスト2-11までの動作を確認

--- ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/Providers/AppServiceProvider.php
+++ ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use App\MyClasses\MyService;
+use App\MyClasses\MyService;
 
 class AppServiceProvider extends ServiceProvider
 {
