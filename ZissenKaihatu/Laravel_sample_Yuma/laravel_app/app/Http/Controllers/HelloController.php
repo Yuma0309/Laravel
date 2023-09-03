ebf6eb97479ab85959a31f4b53ecbf6596ef33ea
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-09-03 13:43:22 +0900

    リスト2-14までの動作を確認

--- ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
+++ ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
@@ -5,11 +5,17 @@ use App\MyClasses\MyService;
 
 class HelloController extends Controller
 {
+    function __construct(MyService $myservice)
+    {
+        $myservice = app('App\MyClasses\MyService');
+    }
+
+
     public function index(MyService $myservice, int $id = -1)
     {
         $myservice->setId($id);
         $data = [
-            'msg' => $myservice->say($id),
+            'msg' => $myservice->say(),
             'data' => $myservice->alldata()
         ];
         return view('hello.index', $data);
