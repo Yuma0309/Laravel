372c635a6d2dc3e2ceea58665766fe22f649c9b5
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-10-10 01:57:55 +0900

    リスト3-15までの動作を確認

--- ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
+++ ZissenKaihatu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
@@ -5,8 +5,9 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index($id)
+    public function index(Request $request)
     {
+        $id = $request->query('page');
         $msg = 'show page: ' . $id;
         $result = DB::table('people')
             ->paginate(3, ['*'], 'page', $id);
