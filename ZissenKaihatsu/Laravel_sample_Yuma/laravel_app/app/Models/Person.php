b6b017a8397bb7c24afaceaf51ad5c9313d204b8
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-11-04 18:10:26 +0900

    リスト3-42までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Models/Person.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Models/Person.php
@@ -51,6 +51,13 @@ class Person extends Model
     {
         $this->attributes['name'] = strtoupper($value);
     }
+
+    public function setAllDataAttribute(Array $value)
+    {
+        $this->attributes['name'] = $value[0];
+        $this->attributes['mail'] = $value[1];
+        $this->attributes['age'] = $value[2];
+    }
 }
 
 
