69a249573f1ccd494ae9dfc1b83dc290ae691a62
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-11-08 00:45:36 +0900

    リスト3-46までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/resources/views/hello/index.blade.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/resources/views/hello/index.blade.php
@@ -2,24 +2,36 @@
 <html lang="ja">
 <head>
     <title>Index</title>
-    <style>
-        th { background-color:red; padding:10px; }
-        td { background-color:#eee; padding:10px; }
-    </style>
     <link href="/css/app.css"  rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    ……必要に応じて記述……
+    <script>
+    function doAction(){
+        var id = document.querySelector('#id').value;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/hello/json/' + id, true);
+        xhr.responseType = 'json';
+        xhr.onload = function(e) {
+            if (this.status == 200) {
+                var result = this.response;
+                document.querySelector('#name').textContent = result.name;
+                document.querySelector('#mail').textContent = result.mail;
+                document.querySelector('#age').textContent = result.age;
+            }
+        };
+        xhr.send();
+    }
+    </script>
 </head>
 <body>
     <h1>Hello/Index</h1>
-    <p>{{$msg}}</p>
-    <table border="1">
-    @foreach($data as $item)
-    <tr>
-        <th>{{$item->id}}</th>
-        <td>{{$item->name_and_age}}</td>
-    </tr>
-    @endforeach
-    </table>
-    <hr>
+    <div>
+        <input type="number" id="id" value="1">
+        <button onclick="doAction();">Click</button>
+    </div>
+    <ul>
+    <li id="name"></li>
+    <li id="mail"></li>
+    <li id="age"></li>
+    </ul>
 </body>
 </html>
