e379a0fa922e62fa1fd3b77897d52ae64463a426
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-11-29 01:37:27 +0900

    リスト4-18までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Storage;
 use App\Models\Person;
 use App\Http\Pagination\MyPaginator;
 use App\Jobs\MyJob;
+use App\Events\PersonEvent;
 
 class HelloController extends Controller
 {
@@ -57,11 +58,13 @@ class HelloController extends Controller
         $id = $request->input('id');
         $person = Person::find($id);
 
-        dispatch(function() use ($person)
-        {
-            Storage::append('person_access_log.txt', 
-                $person->all_data);
-        });
-        return redirect()->route('hello');
+        event(new PersonEvent($person));
+
+        $data = [
+            'input' => '',
+            'msg' => 'id='. $id,
+            'data' => [$person],
+        ];
+        return view('hello.index', $data);
     }
 }
