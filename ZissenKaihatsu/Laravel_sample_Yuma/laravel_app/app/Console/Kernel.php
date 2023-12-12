5f9e9c2f375a7ed034964460d2c7961a24595671
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-12-13 00:00:16 +0900

    リスト4-34までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Console/Kernel.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Console/Kernel.php
@@ -24,15 +24,7 @@ class Kernel extends ConsoleKernel
     {
         $count = Person::all()->count();
         $id = rand(0, $count) + 1;
-
-        /* インスタンス実行
-        $schedule->call(new MyJob($id)); */
-
-        /* ディスパッチする
-        $schedule->call(function() use($id)
-        {
-            MyJob::dispatch($id);
-        }); */
+        $schedule->job(new MyJob($id));
     }
 
     /**
