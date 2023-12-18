860f48b42fb54467a4478f39253c5b76ffa626cc
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-12-18 09:20:37 +0900

    リスト5-7までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/resources/js/app.js
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import ExampleComponent from './components/ExampleComponent.vue';
+import MyComponent from './components/MyComponent.vue';
 
-const app = createApp(ExampleComponent);
+const app = createApp(MyComponent);
 
 app.mount('#app');
