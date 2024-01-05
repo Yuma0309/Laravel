f5e117b51ee254f8ca775fc142df9d3766664e35
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-01-05 10:02:48 +0900

    リスト6-10までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/tests/Feature/ExampleTest.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/tests/Feature/ExampleTest.php
@@ -9,19 +9,14 @@ class ExampleTest extends TestCase
 {
     public function testBasicTest()
     {
-        $this->get('/')->assertStatus(200);
-        $this->get('/hello')->assertOk();
-        $this->post('/hello')->assertStatus(500);
-        $this->get('/hello/1')->assertOk();
-        $this->get('/hoge')->assertStatus(404);
-        $this->get('/hello')->assertSeeText('Index');
-        $this->get('/hello')->assertSee('h1');
-        $this->get('/hello')->assertSeeInOrder(['html','head','body','h1']);
-        $this->get('/hello/json/1')->assertSeeText('YAMADA-TARO');
-        $this->get('/hello/json/2')->assertExactJson(
-            ['id'=>2, 'name'=>'YUMA [+MYJOB]',
-            'mail'=>'yuma@nakamura','age'=> 26,
-            'created_at'=>'2023-11-05T00:30:12.000000Z',
-            'updated_at'=>'2023-12-09T12:34:31.000000Z']);
+        $data = [
+            'id' => 1,
+            'name' => 'YAMADA-TARO',
+            'mail' => 'taro@yamada',
+            'age' => '34'
+        ];
+        $this->assertDatabaseHas('people',$data);
+        $data['id'] =2; //★
+        $this->assertDatabaseMissing('people', $data);
     }
 }
