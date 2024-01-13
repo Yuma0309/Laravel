ea1ba426676ed5be764ff985c3da817969c609a1
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-01-14 01:41:22 +0900

    リスト6-20までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/tests/Feature/ExampleTest.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/tests/Feature/ExampleTest.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factory;
 use Tests\TestCase;
 use Database\Seeders\DatabaseSeeder;
 use App\Models\Person;
+use Illuminate\Support\Facades\Bus;
+use App\Jobs\MyJob;
 
 class ExampleTest extends TestCase
 {
@@ -14,28 +16,20 @@ class ExampleTest extends TestCase
 
     public function testBasicTest()
     {
-        $list = [];
-        for($i = 0;$i < 10;$i++)
-        {
-            $p1 = Person::factory()->create();
-            $p2 = Person::factory()->upper()->create();
-            $p3 = Person::factory()->lower()->create();
-            $p4 = Person::factory()->upper()->lower()->create();
-            $list = array_merge($list, [$p1->id, $p2->id, $p3->id, $p4->id]);
-        }
+        $id = 1;
+        $data = [
+            'id' => $id,
+            'name' => 'DUMMY',
+            'mail' => 'dummy@mail',
+            'age' => 0,
+        ];
+        $person = new Person();
+        $person->fill($data)->save();
+        $this->assertDatabaseHas('people',$data);
 
-        for($i = 0;$i < 10;$i++)
-        {
-            shuffle($list);
-            $item = array_shift($list);
-            $person = Person::find($item);
-            $data = $person->toArray();
-            print_r($data);
-
-            $this->assertDatabaseHas('people', $data);
-
-            $person->delete();
-            $this->assertDatabaseMissing('people', $data);
-        }
+        Bus::fake();
+        Bus::assertNotDispatched(MyJob::class);
+        MyJob::dispatch($id);
+        Bus::assertDispatched(MyJob::class);
     }
 }
