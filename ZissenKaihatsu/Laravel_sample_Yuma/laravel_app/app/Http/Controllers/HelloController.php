f7febbc07d0a03920890619935087e364067ac9f
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-01-31 01:05:03 +0900

    リスト7-8までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Http/Controllers/HelloController.php
@@ -10,26 +10,19 @@ use App\Http\Pagination\MyPaginator;
 use App\Jobs\MyJob;
 use App\Events\PersonEvent;
 use App\MyClasses\PowerMyService;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class HelloController extends Controller
 {
     public function index($id = -1)
     {
-        if ($id> 0)
-        {
-            $msg = 'id = ' . $id;
-            $result = [Person::find($id)];
-        }
-        else
-        {
-            $msg = 'all people data.';
-            $result = Person::get();
-        }
+        $output = new BufferedOutput;
+        Artisan::call('route:list', [], $output);
+        $msg = $output->fetch();
+
         $data = [
             'msg' => $msg,
-            'data' => $result,
         ];
-        // dump($data); //☆
         return view('hello.index', $data);
     }
 
