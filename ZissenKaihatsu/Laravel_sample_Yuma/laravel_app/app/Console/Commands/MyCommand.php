a670e60861f44ef3b2c4fdf06dfcdb38c2463ef3
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-02-13 10:00:43 +0900

    リスト7-22までの動作を確認

--- ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Console/Commands/MyCommand.php
+++ ZissenKaihatsu/Laravel_sample_Yuma/laravel_app/app/Console/Commands/MyCommand.php
@@ -33,32 +33,21 @@ class MyCommand extends Command
      */
     public function handle()
     {
-        $stones = $this->option('stones');
-        $max = $this->option('max');
-        echo "*** start ***\n";
-        while($stones > 0)
+        $choice = ['id', 'name', 'age'];
+        echo "find Person!\n";
+        $field = $this->choice("select field:", $choice, 1);
+        $value = $this->ask('input value:');
+
+        $p = Person::where($field, $value)->first();
+
+        if ($p != null)
         {
-            echo ("stones: $stones\n");
-            $ask = $this->ask("you:");
-            $you = (int)$ask;
-            $you = $you > 0 && $you <= $max ? $you : 1;
-            $stones -= $you;
-            echo ("stones: $stones\n");
-            if ($stones <= 0)
-            {
-                echo "you lose...\n";
-                break;
-            }
-            $me = ($stones - 1) % (1 + $max);
-            $me = $me == 0 ? 1 : $me;
-            $stones -= $me;
-            echo "me: $me\n";
-            if ($stones <= 0)
-            {
-                echo "you win!!\n";
-                break;
-            }
+            echo 'id = ' . $p->id . "\n";
+            echo $p->all_data;
         }
-        echo "--- end ---\n";
+        else
+        {
+            echo "can't find Person.";
+        }
     }
 }
